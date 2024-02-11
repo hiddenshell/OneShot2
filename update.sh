2c82147a289fdd77ac4faf73ed73ab5e821fdd31
hiddenshell <Fuckerboy0099@protonmail.com>
2024-02-12 01:48:46 +0600

    auto

--- update.sh
+++ update.sh
@@ -2,4 +2,5 @@ git config --global user.email "Fuckerboy0099@protonmail.com"
 git config --global user.name "hiddenshell"
 git add --all
 git commit -a -m "auto"
-git push https://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@github.com/hiddenshell/OneShot2.git
+git push https://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
+@github.com/hiddenshell/OneShot2.git
